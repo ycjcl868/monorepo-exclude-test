49a3ae4c01d3ade7b7819c71e9f0616d681f1636
hardfist <1562502418@qq.com>
2019-06-29 21:24:31 +0800

    feat: add divide
    Change-Id: I0d581d7ae102950b4675ee9c75e938ace0cea941

--- packages/ui-lib/src/index.ts
+++ packages/ui-lib/src/index.ts
@@ -6,3 +6,6 @@ export function add(a: number, b: number) {
 export function multi(a:number,b:number){
   return lodash.multiply(a,b);
 }
+export function divide(a:number, b:number){
+  return a/b;
+}
